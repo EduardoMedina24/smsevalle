6fcdc4c82a22e14dd3fa98a3040360f2af6dabb3
EduardoMedina24 <eduardolevm.15@hotmail.com>
2024-11-25 08:38:35 -0500

    primer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>BIENVENIDOS A SMSEVALLE</h1>
+      </header>
+      <main>
+        <p>Sistema de </p>
+      </main>
+    </div>
+  );
+};
+
+export default App;
