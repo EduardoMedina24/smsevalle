3bcc6223f176356bbe651bdcbbf703fdc40e8ddd
Sergio Núñez <sergioop722@gmail.com>
2024-11-26 22:41:52 -0500

    comit:10:41

--- src/components/Navbars/Navbar.js
+++ src/components/Navbars/Navbar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 
 const Navbar = () => {
   return (
@@ -7,26 +7,25 @@ const Navbar = () => {
       <Toolbar>
         {/* Logo */}
         <Box
-  sx={{
-    display: 'flex',
-    alignItems: 'center',
-    mr: 2,
-    backgroundColor: 'white',  // Color de fondo
-    padding: '10px',  // Espaciado alrededor del contenido
-    borderRadius: '8px',  // Bordes redondeados (opcional)
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',  // Sombra (opcional)
-  }}
->
-  <img
-    src="/img/bloque28.png"  // Ruta correcta para la imagen en public/img/
-    alt="Logo"
-    style={{
-      width: 70,  // Ajusta el tamaño del logo
-      height: 'auto',  // Mantén la proporción
-    }}
-  />
-</Box>
-
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            mr: 2,
+            backgroundColor: "white", // Color de fondo
+            padding: "10px", // Espaciado alrededor del contenido
+            borderRadius: "8px", // Bordes redondeados (opcional)
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Sombra (opcional)
+          }}
+        >
+          <img
+            src="/img/bloque28.png" // Ruta correcta para la imagen en public/img/
+            alt="Logo"
+            style={{
+              width: 70, // Ajusta el tamaño del logo
+              height: "auto", // Mantén la proporción
+            }}
+          />
+        </Box>
 
         {/* Nombre o título */}
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -47,4 +46,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
